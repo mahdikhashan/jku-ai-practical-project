9d6e6b540656941b3e09131568357e2466972b09
mahdikhashan <mahdikhashan1@gmail.com>
2025-11-19 16:22:48 +0100

    clean up

--- benchmark/example_throughput.py
+++
@@ -1,9 +0,0 @@
-import torch
-
-model = torch.nn.Linear(2048, 2048).to("cpu")
-
-def data_fn():
-    return torch.randn(64, 2048).to("cpu")
-
-def forward_fn(model, data):
-    return model(data)
--- lizard/__init__.py
+++
@@ -1,3 +0,0 @@
-from .lizard import lizard
-
-__all__ = ["lizard"]
