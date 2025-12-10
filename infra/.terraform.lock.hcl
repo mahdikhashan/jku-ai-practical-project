41455e21d992262d5943e24d2c5b047d3b929f04
mahdikhashan <mahdikhashan1@gmail.com>
2025-12-10 13:01:45 +0100

    add infra scripts

---
+++ infra/.terraform.lock.hcl
@@ -0,0 +1,62 @@
+# This file is maintained automatically by "terraform init".
+# Manual edits may be lost in future updates.
+
+provider "registry.terraform.io/hashicorp/google" {
+  version     = "7.12.0"
+  constraints = ">= 4.0.0"
+  hashes = [
+    "h1:kBKvDUp6GLwHAsoM6CIj9ZTxVBzSnQjyxaVSP8SfqHQ=",
+    "zh:38722ec7777543c23e22e02695e53dd5c94644022647c3c79e11e587063d4d2b",
+    "zh:417b12b69c91c12e3fcefee38744b7a37bae73b706e3071c714151a623a6b0e9",
+    "zh:4902cea92c78b462beaf053de03d0d55fb2241d41ca3379b4568ba247f667fa9",
+    "zh:50ccce39d403ba477943e6652ccb6913092d9dcce1d55533b00b66062888db3d",
+    "zh:56dccfe5df28cfe368d93c37ad6c46a16e76da61482fd0bfc83676b1423cecf5",
+    "zh:7265fca2921e5e300da5d8de7e28b658c0863fdda9da696c5b97dbd3122c17c2",
+    "zh:8317467e828178a6db9ddabe431bb13935c00bfb5e4b4d9760bd56f7ae596eca",
+    "zh:84cc9d9277422a0d6c80d2bd204642d8776ddbba23feb94cf2760bb5f15410bc",
+    "zh:8f79d72e7ed4e36d01560ce5fc944dc7e0387fa0f8272a4345fc6ae896e8f575",
+    "zh:98c3d756beca036f84e7840e2099ff7359e9a246cd9a35386e03ce65032b3f5f",
+    "zh:a07e3ca19673d28da9289ca28dfb83204fa6636f642b8cf46de8caaf526b7dde",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/local" {
+  version     = "2.6.1"
+  constraints = ">= 2.0.0"
+  hashes = [
+    "h1:DbiR/D2CPigzCGweYIyJH0N0x04oyI5xiZ9wSW/s3kQ=",
+    "zh:10050d08f416de42a857e4b6f76809aae63ea4ec6f5c852a126a915dede814b4",
+    "zh:2df2a3ebe9830d4759c59b51702e209fe053f47453cb4688f43c063bac8746b7",
+    "zh:2e759568bcc38c86ca0e43701d34cf29945736fdc8e429c5b287ddc2703c7b18",
+    "zh:6a62a34e48500ab4aea778e355e162ebde03260b7a9eb9edc7e534c84fbca4c6",
+    "zh:74373728ba32a1d5450a3a88ac45624579e32755b086cd4e51e88d9aca240ef6",
+    "zh:78d5eefdd9e494defcb3c68d282b8f96630502cac21d1ea161f53cfe9bb483b3",
+    "zh:8dddae588971a996f622e7589cd8b9da7834c744ac12bfb59c97fa77ded95255",
+    "zh:946f82f66353bb97aefa8d95c4ca86db227f9b7c50b82415289ac47e4e74d08d",
+    "zh:e9a5c09e6f35e510acf15b666fd0b34a30164cecdcd81ce7cda0f4b2dade8d91",
+    "zh:eafe5b873ef42b32feb2f969c38ff8652507e695620cbaf03b9db714bee52249",
+    "zh:ec146289fa27650c9d433bb5c7847379180c0b7a323b1b94e6e7ad5d2a7dbe71",
+    "zh:fc882c35ce05631d76c0973b35adde26980778fc81d9da81a2fade2b9d73423b",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version     = "4.1.0"
+  constraints = ">= 4.0.0"
+  hashes = [
+    "h1:zEv9tY1KR5vaLSyp2lkrucNJ+Vq3c+sTFK9GyQGLtFs=",
+    "zh:14c35d89307988c835a7f8e26f1b83ce771e5f9b41e407f86a644c0152089ac2",
+    "zh:2fb9fe7a8b5afdbd3e903acb6776ef1be3f2e587fb236a8c60f11a9fa165faa8",
+    "zh:35808142ef850c0c60dd93dc06b95c747720ed2c40c89031781165f0c2baa2fc",
+    "zh:35b5dc95bc75f0b3b9c5ce54d4d7600c1ebc96fbb8dfca174536e8bf103c8cdc",
+    "zh:38aa27c6a6c98f1712aa5cc30011884dc4b128b4073a4a27883374bfa3ec9fac",
+    "zh:51fb247e3a2e88f0047cb97bb9df7c228254a3b3021c5534e4563b4007e6f882",
+    "zh:62b981ce491e38d892ba6364d1d0cdaadcee37cc218590e07b310b1dfa34be2d",
+    "zh:bc8e47efc611924a79f947ce072a9ad698f311d4a60d0b4dfff6758c912b7298",
+    "zh:c149508bd131765d1bc085c75a870abb314ff5a6d7f5ac1035a8892d686b6297",
+    "zh:d38d40783503d278b63858978d40e07ac48123a2925e1a6b47e62179c046f87a",
+    "zh:f569b65999264a9416862bca5cd2a6177d94ccb0424f3a4ef424428912b9cb3c",
+    "zh:fb07f708e3316615f6d218cec198504984c0ce7000b9f1eebff7516e384f4b54",
+  ]
+}
--- infra/main.tf
+++ infra/main.tf
@@ -4,18 +4,87 @@ terraform {
       source  = "hashicorp/google"
       version = ">= 4.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 4.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = ">= 2.0"
+    }
   }
 }
 
-variable "project_id" {}
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
+
 variable "zone" {
-  default = "us-west4-a"
+  description = "GCP Zone"
+  type        = string
+  default     = "asia-southeast1-a"
 }
+
 variable "region" {
-  default = "us-west4"
+  description = "GCP Region"
+  type        = string
+  default     = "asia-southeast1"
 }
-variable "repo_url" {}
-variable "script_path" {}
+
+variable "instance_name" {
+  description = "Name of the VM instance"
+  type        = string
+  default     = "temp-gpu-vm"
+}
+
+variable "machine_type" {
+  description = "Machine type for the VM"
+  type        = string
+  default     = "g2-standard-4"
+}
+
+variable "boot_disk_image" {
+  description = "Boot disk image"
+  type        = string
+  default     = "deeplearning-platform-release/pytorch-2-7-cu128-ubuntu-2404-nvidia-570-v20251209"
+}
+
+variable "network" {
+  description = "Network name"
+  type        = string
+  default     = "default"
+}
+
+variable "gpu_type" {
+  description = "GPU accelerator type"
+  type        = string
+  default     = "nvidia-l4"
+}
+
+variable "gpu_count" {
+  description = "Number of GPUs"
+  type        = number
+  default     = 1
+}
+
+variable "on_host_maintenance" {
+  description = "Host maintenance policy"
+  type        = string
+  default     = "TERMINATE"
+}
+
+variable "automatic_restart" {
+  description = "Whether to automatically restart VM"
+  type        = bool
+  default     = true
+}
+
+variable "install_nvidia_drivers" {
+  description = "Whether to install NVIDIA drivers"
+  type        = string
+  default     = "true"
+}
 
 provider "google" {
   project = var.project_id
@@ -23,42 +92,70 @@ provider "google" {
   zone    = var.zone
 }
 
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${path.module}/ssh_key"
+  file_permission = "0600"
+}
+
+resource "local_file" "public_key" {
+  content         = tls_private_key.ssh_key.public_key_openssh
+  filename        = "${path.module}/ssh_key.pub"
+  file_permission = "0644"
+}
+
 resource "google_compute_instance" "gpu_vm" {
-  name         = "temp-gpu-vm"
-  machine_type = "n1-standard-4"
+  name         = var.instance_name
+  machine_type = var.machine_type
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      image = var.boot_disk_image
     }
   }
 
   network_interface {
-    network = "default"
+    network = var.network
     access_config {}
   }
 
   guest_accelerator {
-    type  = "nvidia-tesla-t4"
-    count = 1
+    type  = var.gpu_type
+    count = var.gpu_count
   }
 
   scheduling {
-    on_host_maintenance = "TERMINATE"
-    automatic_restart   = true
+    on_host_maintenance = var.on_host_maintenance
+    automatic_restart   = var.automatic_restart
   }
 
   metadata = {
-    install-nvidia-drivers = "true"
+    install-nvidia-drivers = var.install_nvidia_drivers
+    ssh-keys              = "ubuntu:${tls_private_key.ssh_key.public_key_openssh}"
   }
+}
 
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    set -e
-    apt-get update
-    apt-get install -y git python3 python3-pip
-    git clone ${var.repo_url} /tmp/repo
-    cd /tmp/repo
-    python3 ${var.script_path} || true
-  EOT
+output "instance_name" {
+  description = "Name of the created instance"
+  value       = google_compute_instance.gpu_vm.name
+}
+
+output "instance_external_ip" {
+  description = "External IP address of the instance"
+  value       = google_compute_instance.gpu_vm.network_interface[0].access_config[0].nat_ip
+}
+
+output "instance_internal_ip" {
+  description = "Internal IP address of the instance"
+  value       = google_compute_instance.gpu_vm.network_interface[0].network_ip
+}
+
+output "ssh_command" {
+  description = "SSH command to connect to the instance"
+  value       = "ssh -i ssh_key ubuntu@${google_compute_instance.gpu_vm.network_interface[0].access_config[0].nat_ip}"
 }
